58e280b2affa6bd1d5d2a7483270b036632a17f0
eNNdy <olya.khomy.tska.03@gmail.com>
2023-09-24 13:36:01 +0300

    Update index.js

--- index.js
+++ index.js
@@ -35,7 +35,13 @@ function makeid(length) {
     }
     return result;
 }
+process.on('uncaughtException', function (error) {
+	console.log("\x1b[31m", "Exception: ", error, "\x1b[0m");
+});
 
+process.on('unhandledRejection', function (error, p) {
+	console.log("\x1b[31m","Error: ", error.message, "\x1b[0m");
+});
 // Async fanction for ordering
 const handleOrderThem = async (msg) => {
     const chatId = msg.chat.id;
